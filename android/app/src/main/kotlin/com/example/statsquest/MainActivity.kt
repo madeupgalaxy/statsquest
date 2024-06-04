5244db1608b3690455ded760317f6fcff1248e01
Rahil <118007944+madeupgalaxy@users.noreply.github.com>
2024-06-04 23:54:22 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/statsquest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.statsquest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Breakfast',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          height: 30,
+          width: 30,
+          decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10)),
+          child: SvgPicture.asset('assets/icons/Arrow.svg'),
+        ),
+      ),
+    );
+  }
+}
